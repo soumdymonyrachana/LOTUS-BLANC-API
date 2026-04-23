4c1b153f78829b2cda90f96d892ed0a947fd01f2
Rachana <soum.dymonyrachana@institute.pse.ngo>
2026-04-23 15:35:38 +0700

    update

--- src/controllers/dishController.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-export declare const getDishes: (_req: Request, res: Response) => Promise<void>;
-export declare const getDishById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const postDish: (req: Request, res: Response) => Promise<void>;
-export declare const updateDish: (req: Request, res: Response) => Promise<void>;
-export declare const removeDish: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=dishController.d.ts.map
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`API documentation available at http://localhost:${PORT}/api-docs`);
-});
-//# sourceMappingURL=index.js.map
