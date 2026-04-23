e1a55a4012f8a55e4e294f8f1bfd2c441ba47bcf
Rachana <soum.dymonyrachana@institute.pse.ngo>
2026-04-23 15:35:38 +0700

    update

--- 222prisma.config.d.ts
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-declare const _default: any;
-export default _default;
-//# sourceMappingURL=222prisma.config.d.ts.map
--- src/controllers/categoryController.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from 'express';
-export declare const getCategories: (_req: Request, res: Response) => Promise<void>;
-export declare const postCategory: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const putCategory: (req: Request, res: Response) => Promise<void>;
-export declare const removeCategory: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=categoryController.d.ts.map
